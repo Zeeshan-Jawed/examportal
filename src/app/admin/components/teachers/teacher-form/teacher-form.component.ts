e3f85d6665397dcf49308502408d393d00e328b6
Zeeshan <jawedzeeshan1@gmail.com>
2020-12-02 19:25:19 +0500

    Teacher Component

--- src/app/admin/components/teachers/teacher-form/teacher-form.component.ts
+++ src/app/admin/components/teachers/teacher-form/teacher-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-teacher-form',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./teacher-form.component.css']
 })
 export class TeacherFormComponent implements OnInit {
-
+  hide=true;
+  name=null
   constructor() { }
 
   ngOnInit(): void {
   }
+  email = new FormControl('', [Validators.required, Validators.email]);
 
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 }
